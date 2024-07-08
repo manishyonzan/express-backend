7918bbe475a35769f84df20ee5c743053a6e7e68
Manish Lama <lmanish931@gmail.com>
2024-07-08 22:13:36 +0545

    minor change

--- controller/order/order.js
+++ controller/order/order.js
@@ -3,15 +3,15 @@ const AppError = require("../../utils/appError");
 
 
 const orderController = {
-    getOrder: async (req, res,next) => {
+    getOrder: async (req, res, next) => {
         try {
             const { id } = req.params;
             console.log(id);
             const response = await orderRepository.getOrders(id);
             if (response) {
-                res.status(200).send(response);
+                res.status(200).send({ success: true, data: response, error: null});
             }
-            throw new AppError("Something Went wrong",401);
+            throw new AppError("Something Went wrong", 401);
 
         } catch (error) {
             next(error);
