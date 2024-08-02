0b87735a2c2d1220bc595af3c24bff7a8b0f8908
Manish Lama <lmanish931@gmail.com>
2024-08-02 20:54:31 +0545

    rate limiter added

--- routes/product.js
+++ routes/product.js
@@ -4,13 +4,14 @@ const { getAdminFromToken } = require("../middleware/getAdminFromToken");
 const productService = require("../service/product/productService");
 const { getUserFromToken } = require("../middleware/getUserFromToken");
 const orderController = require("../controller/order/order");
+const rateLimiter = require("../middleware/rateLimitter");
 
 const router = express.Router();
 
-router.get("/get", productController.getProduct);
+router.get("/get", rateLimiter, productController.getProduct);
 router.post("/create", getAdminFromToken, productService.formatProductService, productController.createProduct)
 router.delete("/delete/:productId", getUserFromToken, productController.deleteProduct);
-router.patch("/update/:productId",getAdminFromToken,productController.updateProduct);
-router.get("/email",orderController.sendMessage);
+router.patch("/update/:productId", getAdminFromToken, productController.updateProduct);
+router.get("/email", orderController.sendMessage);
 
 module.exports = router;
